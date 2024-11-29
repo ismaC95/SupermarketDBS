e619dca05d7027f109af9ab131e476bb738aa8c9
Admin <Admin@8TGF593-QBK.inc.bytedance.com>
2024-11-29 16:59:02 +0000

    Navigation narrow not working

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//     if (jQuery) {
+// 		alert("jQuery loaded");
+// 	}
+// });
+
+$(document).ready(function(){
+    $("#image2").on("click", function(){
+        $(".narrow-nav-pages").toggle(500);
+    });
+});
